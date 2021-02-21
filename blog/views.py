8762a06ac7dc43a3a2d3bddc60862edb64decac5
balajisunder <balaji.sunder@gmail.com>
2021-02-21 02:12:21 -0600

    v0.1

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views import generic
 
-# Create your views here.
+class PostListView(generic.ListView):
+    # queryset = Post.published.all()
+    template_name = 'blog/post/list.html'
+    context_object_name = 'post_list'
+    paginate_by = 3
+
+    def get_queryset(self):
+        return Post.objects.filter(status='published')
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month,
+                             publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
